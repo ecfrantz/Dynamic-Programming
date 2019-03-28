e31d59e8abdf7616bc8a5afe5983dec4606817a0
Matthew Miller <matthewm8m@gmail.com>
2019-03-28 06:59:25 -0600

    abstract loading input to method

--- DynamicProgramming/main.cpp
+++ DynamicProgramming/main.cpp
@@ -1,24 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <istream>
 #include <fstream>
+#include <stdlib.h>
 
-int main() {
-    vector<int> x;
-    vector<int> s;
-    ifstream inFile(test);
-    if(inFile.fail()){
-        cerr << "Error opening input file";
+using namespace std;
+
+class ProblemInput
+{
+    public:
+    vector<int> X;
+    vector<int> S;
+
+    ProblemInput(vector<int> X, vector<int> S)
+    {
+        this->X = X;
+        this->S = S;
+    }
+};
+
+
+ProblemInput loadInput(string path)
+{
+    ifstream inFile(path);
+
+    if (inFile.fail())
+    {
+        cerr << "Error opening input file.";
         exit(1);
     }
-    int xval;
-    int sval;
-    while(inFile.eof() == false){
-        inFile >> xval;
-        x.push_back(xval);
-        inFile >> sval;
-        s.push_back(sval);
+
+    int n;
+    inFile >> n;
+
+    vector<int> X;
+    vector<int> S;
+    X.reserve(n);
+    S.reserve(n);
+
+    int x;
+    int s;
+    for (int i = 0; i < n && !inFile.eof(); i++)
+    {
+        inFile >> x;
+        inFile >> s;
+
+        X.push_back(x);
+        S.push_back(s);
     }
-    std::cout << "Hello, World!" << std::endl;
+
+    inFile.close();
+
+    return ProblemInput(X, S);
+}
+
+
+int main(int argn, char** args)
+{
+    string inputPath = "input.txt";
+    ProblemInput input = loadInput(inputPath);
+
     return 0;
 }
