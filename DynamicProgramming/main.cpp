6c2d952cacfaf52c07a932b24093e7d9447117db
Matthew Miller <matthewm8m@gmail.com>
2019-03-29 09:17:28 -0600

    add documentation

--- DynamicProgramming/main.cpp
+++ DynamicProgramming/main.cpp
@@ -110,20 +110,30 @@ int** calculateSigma(ProblemInput input)
 /// Calculuate M (score array) via dynamic programming. 
 SolutionNode* calculateScores(ProblemInput input, int** sigma)
 {
+    // Allocate space for the score array for n days
 	SolutionNode* M = new SolutionNode[input.n];
 
+    // Solve for M[q] (index corrected) for q = 1 to n (complete score array)
 	for (int q = 0; q < input.n; q++)
 	{
+        // Initialize maximum score trackers
 		int maxScore = 0;
 		int maxIndex = -1;
+
+        // M[q] is the maximum of the scores assuming we reboot on day i
+        // or we do not reboot at all. 
 		for (int i = 0; i <= q; i++)
 		{
+            // Get base score, M[i - 1] (index corrected), where M[-1] = M[0] = 0
 			int score = (i <= 1) ? 0 : M[i - 2].score;
+
+            // Add up scores of days after reboot
 			for (int j = i; j <= q; j++)
 			{
 				score += min(input.X[j], input.S[j - i]);
 			}
 
+            // There is a new maximum score so record where it was found 
 			if (score > maxScore)
 			{
 				maxScore = score;
@@ -131,27 +141,42 @@ SolutionNode* calculateScores(ProblemInput input, int** sigma)
 			}
 		}
 
+        // Set maximum score for first q days where there is a reboot on day i 
 		M[q].score = maxScore;
 		M[q].parent = maxIndex - 1;
 	}
+
 	return M;
 }
 
+/// Calculate the traceback of the algorithm (the throughput of each day).
 int* calculateTraceback(ProblemInput input, SolutionNode* M)
 {
+    // Allocate space for the traceback.
 	int* throughput = new int[input.n];
 
+    // Initialize two pointers (i = low pointer), (j = high pointer)
 	int i;
 	int j = input.n;
 	SolutionNode node;
+
+    // Walk from end to beginning calculating the throughput.
+    // Index j indicates the day we are calculating.
+    // Index i indicates the most recent reboot.
+    // Initializes to parent of M[n].
 	while (j > 0)
 	{
+        // Set the day before the latest reboot.
 		node = M[--j];
 		i = node.parent;
+
+        // Set days after and until reboot to throughput.
 		for (; j > i; j--)
 		{
 			throughput[j] = min(input.X[j], input.S[j - i - 1]);
 		}
+
+        // Set day of reboot to 0.
 		throughput[j] = 0;
 	}
 
@@ -159,6 +184,7 @@ int* calculateTraceback(ProblemInput input, SolutionNode* M)
 }
 
 
+/// Entry point to the program
 int main(int argn, char** args)
 {
 	// Get input from file.
